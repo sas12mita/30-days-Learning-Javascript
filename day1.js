048973264e5f41d09ae6d68c3992db36f910f506
sas12mita <sasmitapoudel.projectwork@gmail.com>
2025-06-15 16:42:09 +0545

    day1[variable declaration,scope checking]

--- day1.js
+++ day1.js
@@ -1,6 +1,21 @@
-let name = "Sasmita";    // String (text)
+{let fullname = "Sasmita";    // String (text)
 let age = 25;            // Number
-const pi = 3.14;    
-console.log(name);
+const pi = 3.14;  
+var ag=45  
+console.log(fullname);
 console.log(age);
 console.log(pi);
+console.log(typeof(fullname));
+console.log(typeof(age));
+console.log(typeof(pi));
+}
+console.log(ag);
+function test()
+{
+    const ag=23;
+    var ah=898;
+    let age=45;
+
+}
+console.log(ag)// checking scope
+console.log(age)
