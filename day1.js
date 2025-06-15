86e12daa91160f64ab43aad9dd208a0a10ec5469
sas12mita <sasmitapoudel.projectwork@gmail.com>
2025-06-15 16:53:29 +0545

    object

--- day1.js
+++ day1.js
@@ -1,4 +1,4 @@
-{let fullname = "Sasmita";    // String (text)
+let fullname = "Sasmita";    // String (text)
 let age = 25;            // Number
 const pi = 3.14;  
 var ag=45  
@@ -8,14 +8,30 @@ console.log(pi);
 console.log(typeof(fullname));
 console.log(typeof(age));
 console.log(typeof(pi));
-}
-console.log(ag);
-function test()
-{
-    const ag=23;
-    var ah=898;
-    let age=45;
+// }
+// console.log(ag);
+// function test()
+// {
+//     const ag=23;
+//     var ah=898;
+//     let age=45;
 
-}
-console.log(ag)// checking scope
-console.log(age)
+// }
+// console.log(ag)// checking scope
+// console.log(age)
+
+const person=[
+    {
+        'name':"sasmita",
+        'age':12,
+        'city':'pokhara'
+    },
+    {
+        'name':"Bikash",
+        'age':24,
+        'city':'kathmandu',
+    }
+]
+    
+console.log(person[1].city);
+console.log(person);
