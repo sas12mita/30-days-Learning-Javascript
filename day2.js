21636b16023e581983260a21aef4e535b0e3f7dd
sas12mita <sasmitapoudel.projectwork@gmail.com>
2025-06-18 07:43:30 +0545

    airthmetic, comparision ,logical, and assignment operator

---
+++ day2.js
@@ -0,0 +1,31 @@
+//arithmetic operation
+console.log(5+10);
+console.log(5-10);
+console.log(5*10);
+console.log(5/10);
+console.log(5*2);
+let counter=0;
+console.log(++counter);
+console.log(counter++);
+console.log(counter++);
+console.log(counter++);
+console.log(counter++);
+const a="5";
+const b=5;
+console.log(a===b);
+console.log(a==b);
+//Comparison Operators
+console.log('\nComparison:');
+console.log('5 == "5"', 5 == '5');   // true (loose equality)
+console.log('5 === "5"', 5 === '5'); // false (strict equality)
+console.log('5 != "5"', 5 != '5');   // false
+console.log('5 !== "5"', 5 !== '5'); // true
+console.log('5 > 3', 5 > 3);         // true
+console.log('5 < 3', 5 < 3)
+// Implicit Coercion (Automatic)
+console.log(5+true);
+console.log(5+false);
+console.log("TEXT"+null)
+const text=0;
+
+console.log('text:',0)
