e6d9526196ae7e21f43f06b77d3fee025a2025cf
sas12mita <sasmitapoudel.projectwork@gmail.com>
2025-06-18 13:10:18 +0545

    Boolean , Inp\mplicit and EXplicit Conversion

--- day2.js
+++ day2.js
@@ -22,10 +22,35 @@ console.log('5 != "5"', 5 != '5');   // false
 console.log('5 !== "5"', 5 !== '5'); // true
 console.log('5 > 3', 5 > 3);         // true
 console.log('5 < 3', 5 < 3)
+
+
+//logical operator
+console.log('\nLogical Operators:')
+console.log('true && true', true && true);
+console.log('true && false', true && false);
+console.log('false && true', false && true);
+console.log('false && false', false && false);
+console.log('\nLogical Operators:')
+console.log('true ||true', true || true);
+console.log('true ||false', true || false);
+console.log('false|| true', false || true);
+console.log('false|| false', false || false);
+
+//Boolean check
+console.log('\nBoolean Check:')
+console.log(Boolean(1));
+console.log(Boolean(0));
+console.log(Boolean(10))
+console.log(Boolean("hi"))
 // Implicit Coercion (Automatic)
 console.log(5+true);
 console.log(5+false);
 console.log("TEXT"+null)
-const text=0;
+const text=20;
+console.log(Boolean(text));// console.log('text:',0)
 
-console.log('text:',0)
+// Explicit Conversion (Manual)
+console.log(Number(true));
+console.log(Number(false));
+console.log(Number("hi"));
+console.log(String(1))
