440108866e836a68575c953ed7dea5bb76635d07
sas12mita <sasmitapoudel.projectwork@gmail.com>
2025-06-19 16:51:14 +0545

    function

---
+++ day4-function.js
@@ -0,0 +1,22 @@
+function add()
+{
+    console.log('hellow')
+   return "hello"
+}
+console.log(add())
+
+const greet= function(name)
+{
+    return name
+}
+console.log(greet('sasmita'))
+const greet2=(name)=>
+{
+    return name
+}
+console.log(greet2('bikash'));
+
+function greet3(name) {
+  return "Hello " + name;
+}
+console.log(greet3('sasmia'));
