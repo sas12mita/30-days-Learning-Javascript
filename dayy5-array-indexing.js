7f1a76c3d2c069e4c41b5a94546455fe7981e83c
sas12mita <sasmitapoudel.projectwork@gmail.com>
2025-06-21 20:13:13 +0545

    array and indexing

---
+++ dayy5-array-indexing.js
@@ -0,0 +1,9 @@
+const fruits=["mango","banana","grapes"];
+const fruit=new Array(1,2,3,4,);
+//
+// console.log(fruit[2]);
+//console.log(fruits.push('kaju'));
+fruits.push('leeche');
+fruits.pop('leeche');
+
+console.log(fruits);
