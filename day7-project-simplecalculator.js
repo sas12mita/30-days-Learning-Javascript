0dfd5897055186513b254f00774a2f3571fddca3
sas12mita <sasmitapoudel.projectwork@gmail.com>
2025-06-29 13:02:47 +0545

    day 9 DOM basic

--- day7-project-simplecalculator.js
+++ day7-project-simplecalculator.js
@@ -1,4 +1,25 @@
-const a=10;
-const b=20;
-const selectoperation=[1,2,3,4,5];
-console.log(1);
+const a = 10;
+const b = 20;
+const Calculator = {
+    add: function () {
+        return a + b;
+    },
+    subtract: function (c, d) {
+        sub = c - d
+        return sub
+    },
+    multiple: function () {
+        return a * b;
+    },
+    divide: function () {
+       return a / b;
+    }
+}
+
+const addoperation = Calculator.add();
+const suboperation = Calculator.subtract(30, 10);
+const multipleopration = Calculator.multiple();
+console.log(addoperation);
+console.log(suboperation);
+console.log(multipleopration);
+console.log(Calculator.divide());
