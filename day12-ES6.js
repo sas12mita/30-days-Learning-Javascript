621471a7cf6aab0906ea3fc43bba80ec287415ba
sas12mita <sasmitapoudel.projectwork@gmail.com>
2025-07-04 13:08:06 +0545

    project-TODO app

--- day12-ES6.js
+++ day12-ES6.js
@@ -24,3 +24,15 @@ const arr1=[1,2,3]
 const arr2=[...arr1,4,5,6]
 console.log(arr2)
 
+
+//arrow function
+ const add=()=>{
+    return 5+5
+ }
+ console.log(add())
+
+ function sub()
+ {
+    return 5-5
+ }
+ console.log(sub())
